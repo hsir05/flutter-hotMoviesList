07b07f19174c2bae7787a6437a4a4bb66c605042
hdq <haqsir@163.com>
2019-11-05 22:10:39 +0800

    更新dio， 请求方式统一

---
+++ lib/config/service_url.dart
@@ -0,0 +1,4 @@
+const serviceUrl= 'https://miniapp.you.163.com';
+const servicePath={
+  'homePageContext': serviceUrl+'/xhr/index/index.json', 
+};
---
+++ lib/service/service_method.dart
@@ -0,0 +1,30 @@
+import "package:dio/dio.dart";
+import 'dart:async';
+import 'dart:io';
+import '../config/service_url.dart';
+
+Future request(url,formData)async{
+    try{
+      print('$url, 开始获取数据...............');
+      Response response;
+      Dio dio = new Dio();
+      dio.options.contentType=ContentType.parse("application/x-www-form-urlencoded");
+      if(formData==null){
+          response = await dio.post(servicePath[url]);
+      }else{
+          response = await dio.post(servicePath[url],data:formData);
+      }
+      if(response.statusCode==200){
+        // if (url == 'recItemDetailById'){
+        //   print('++++++++++++++recItemDetailById+++++++++++++');
+        //   print(response.data);
+        // }
+        return response.data;
+      }else{
+          throw Exception('后端接口出现异常，请检测代码和服务器情况.........');
+      }
+    }catch(e){
+        return print('ERROR:======>$e');
+    }
+     
+}
