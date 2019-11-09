446ae08c0f96db8f15168a4bb6ae73b7b632287e
hdq <haqsir@163.com>
2019-11-10 00:46:11 +0800

    优雅正在热映代码， 请求报错

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:movies/res/resources.dart';
+import 'package:movies/constant/constant.dart';
+import 'package:movies/widget/radius_img.dart';
+import 'package:movies/widget/video_widget.dart';
+
 import './searchBar.dart';
 import '../service/service_method.dart';
 import '../model/hot_model.dart';
+
+ final List<Tab> myTabs = <Tab>[
+    Tab(text: '正在热映'),
+    Tab(text: '即将上映'),
+  ];
 
 class HomePage extends StatefulWidget {
   @override
@@ -11,10 +20,10 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage>with SingleTickerProviderStateMixin {
-  final List<Tab> myTabs = <Tab>[
-    Tab(text: '正在热映'),
-    Tab(text: '即将上映'),
-  ];
+  // final List<Tab> myTabs = <Tab>[
+  //   Tab(text: '正在热映'),
+  //   Tab(text: '即将上映'),
+  // ];
 
   List<Subject> list = [];
 
@@ -26,19 +35,18 @@ class _HomePageState extends State<HomePage>with SingleTickerProviderStateMixin 
     super.initState();
     // _getData();
     _tabController = TabController(vsync: this, length: myTabs.length);
-    // _tabController.addListener(() => _onTabChanged());
+    _tabController.addListener(() => _onTabChanged());
   }
 
-     /// tab改变监听
-  // _onTabChanged() {
-  //   if (_tabController.index.toDouble() == _tabController.animation.value) {
-  //     //赋值 并更新数据
-  //     // this.setState(() {
-  //     //   _currentIndex = _tabController.index;
-  //     // });
-  //     // _getData();
-  //   }
-  // }
+  _onTabChanged() {
+    if (_tabController.index.toDouble() == _tabController.animation.value) {
+      //赋值 并更新数据
+      // this.setState(() {
+      //   _currentIndex = _tabController.index;
+      // });
+      // _getData();
+    }
+  }
 
     void _getData(){
     request('hotPageContext', null).then((result){
@@ -60,7 +68,8 @@ class _HomePageState extends State<HomePage>with SingleTickerProviderStateMixin 
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colours.bg_color,
+        // backgroundColor: Colours.bg_color,
+        backgroundColor: Colors.white,
         leading: Container(
           margin: EdgeInsets.only(left: 4.0),
           alignment: Alignment.center,
@@ -103,23 +112,29 @@ class _HomePageState extends State<HomePage>with SingleTickerProviderStateMixin 
         controller: _tabController,
         // children: _wrapList(context, list)
         children: myTabs.map((Tab tab) {
-          return Center(child: Text(tab.text));
+          // return Center(child: Text(tab.text));
+          // return _wrapList(context);
+         return SliverContainer(
+            name: tab.text,
+          );
         }).toList(),
       )
     );
   }
 
-   _wrapList(context, list) {
-    if (list.length!= 0){
-      List<Widget>listWidget = list.map((val){
-          return getItem(val);
-      }).toList();
-       return Wrap(
-        spacing: 2,
-        children: listWidget,
-      );
-    }
-  }
+  //  _wrapList(context) {
+  //   if (list.length!= 0){
+  //     List<Widget>listWidget = list.map((val){
+  //         return getItem(val);
+  //     }).toList();
+  //      return Wrap(
+  //       spacing: 2,
+  //       children: listWidget,
+  //     );
+  //   } else {
+  //     return Text('data');
+  //   }
+  // }
 
   getItem(var subject) {
     //    演员列表
@@ -213,5 +228,241 @@ class _HomePageState extends State<HomePage>with SingleTickerProviderStateMixin 
         )
         );
   }
+}
+
+
+class SliverContainer extends StatefulWidget {
+  final String name;
+
+  SliverContainer({Key key, @required this.name}) : super(key: key);
+
+  @override
+  _SliverContainerState createState() => _SliverContainerState();
+}
+
+class _SliverContainerState extends State<SliverContainer> {
+  @override
+  void initState() {
+    super.initState();
+    print('init state${widget.name}');
+
+    ///请求动态数据
+    if (list == null || list.isEmpty) {
+      if (myTabs[0].text == widget.name) {
+        requestAPI();
+      } else {
+        ///请求推荐数据
+        requestAPI();
+      }
+    }
+  }
+
+  List<Subject> list;
+
+  void requestAPI(){
+    request('hotPageContext', null).then((result){
+      print('+++++++++++++');
+      print(result['subjects']);
+        var resultList = result['subjects'];
+        list = resultList.map<Subject>((item) => Subject.fromMap(item)).toList();
+        // print(list);
+        setState(() {});
+    }); 
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return getContentSliver(context, list);
+  }
+
+  getContentSliver(BuildContext context, List<Subject> list) {
+    print('==========================');
+    print(widget.name);
+    print(myTabs[0].text);
+    if (widget.name == myTabs[0].text) {
+      return _loginContainer(context);
+    }
+
+    print('getContentSliver');
+    if (list == null || list.length == 0) {
+      print(333);
+      return Text('暂无数据');
+    }
+    return SafeArea(
+      top: false,
+      bottom: false,
+      child: Builder(
+        builder: (BuildContext context) {
+          return CustomScrollView(
+            physics: const BouncingScrollPhysics(),
+            key: PageStorageKey<String>(widget.name),
+            slivers: <Widget>[
+              SliverOverlapInjector(
+                handle:
+                    NestedScrollView.sliverOverlapAbsorberHandleFor(context),
+              ),
+              SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                      ((BuildContext context, int index) {
+                return getCommonItem(list, index);
+              }), childCount: list.length)),
+            ],
+          );
+        },
+      ),
+    );
+  }
+
+  double singleLineImgHeight = 180.0;
+  double contentVideoHeight = 350.0;
+
+  ///列表的普通单个item
+  getCommonItem(List<Subject> items, int index) {
+    Subject item = items[index];
+    bool showVideo = index == 1 || index == 3;
+    return Container(
+      height: showVideo ? contentVideoHeight : singleLineImgHeight,
+      color: Colors.white,
+      margin: const EdgeInsets.only(bottom: 10.0),
+      padding: const EdgeInsets.only(
+          left: 13.0,
+          right: 13.0,
+          top: 13.0,
+          bottom: 10.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 25.0,
+                backgroundImage: NetworkImage(item.casts[0].avatars.medium),
+                backgroundColor: Colors.white,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10.0),
+                child: Text(item.title),
+              ),
+              Expanded(
+                child: Align(
+                  child: Icon(
+                    Icons.more_horiz,
+                    color: Colors.grey,
+                    size: 18.0,
+                  ),
+                  alignment: Alignment.centerRight,
+                ),
+              )
+            ],
+          ),
+          Expanded(
+              child: Container(
+            child: showVideo ? getContentVideo(index) : getItemCenterImg(item),
+          )),
+          Padding(
+            padding: const EdgeInsets.only(left: 15.0, right: 15.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                 Image.asset(
+          'images/ic_new_empty_view_default.png',
+          width: 120.0,
+        ),
+                Image.asset(
+                  'images/ic_vote.png',
+                  width: 25.0,
+                  height: 25.0,
+                ),
+                Image.asset(
+                  'images/ic_notification_tv_calendar_comments.png',
+                  width: 20.0,
+                  height: 20.0,
+                ),
+                Image.asset(
+                  'images/ic_status_detail_reshare_icon.png',
+                  width: 25.0,
+                  height: 25.0,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  getItemCenterImg(Subject item) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: <Widget>[
+        Expanded(
+          child: RadiusImg.get(item.images.large, null,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(5.0),
+                    bottomLeft: Radius.circular(5.0)),
+              )),
+        ),
+        Expanded(
+          child: RadiusImg.get(item.casts[1].avatars.medium, null, radius: 0.0),
+        ),
+        Expanded(
+          child: RadiusImg.get(item.casts[2].avatars.medium, null,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(5.0),
+                      bottomRight: Radius.circular(5.0)))),
+        )
+      ],
+    );
+  }
+
+  getContentVideo(int index) {
+    if(!mounted){
+      return Container();
+    }
+    return VideoWidget(
+      index == 1 ? Constant.URL_MP4_DEMO_0 :  Constant.URL_MP4_DEMO_1,
+      showProgressBar: false,
+    );
+  }
+}
+///动态TAB
+_loginContainer(BuildContext context) {
+  return Align(
+    alignment: Alignment(0.0, 0.0),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Image.asset(
+          'images/ic_new_empty_view_default.png',
+          width: 120.0,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 15.0, bottom: 25.0),
+          child: Text(
+            '登录后查看关注人动态',
+            style: TextStyle(fontSize: 16.0, color: Colors.grey),
+          ),
+        ),
+        GestureDetector(
+          child: Container(
+            child: Text(
+              '去登录',
+              style: TextStyle(fontSize: 16.0, color: Colors.green),
+            ),
+            padding: const EdgeInsets.only(
+                left: 35.0, right: 35.0, top: 8.0, bottom: 8.0),
+            decoration: BoxDecoration(
+                border: Border.all(color: Colors.green),
+                borderRadius: const BorderRadius.all(Radius.circular(6.0))),
+          ),
+          onTap: () {
+            // Router.push(context, Router.searchPage, '搜索笨啦灯');
+          },
+        )
+      ],
+    ),
+  );
 }
 
